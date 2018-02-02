7f3b3db7152d2ed9dd62516b9f8efd4b30dfac31
Guillaume Serin <guillaume.serin@viacesi.fr>
2018-02-02 11:25:02 +0100

    ajout deposer, retirer, virer

--- git_cours/src/banque/Compte.java
+++ git_cours/src/banque/Compte.java
@@ -1,5 +1,22 @@
 package banque;
 
 public class Compte {
-	private float solde = 0;
+	private int solde = 0;
+	
+	public void deposer(int somme) {
+		this.solde += somme;
+	}
+	
+	public int retirer(int somme) {
+		int res = this.solde-somme;
+		this.solde-= somme;
+		return res;
+	}
+	
+	public int virer(int somme, Compte compteDestination) {
+		int res = somme;
+		this.solde-=somme;
+		compteDestination.deposer(somme);
+		return res;
+	}
 }
